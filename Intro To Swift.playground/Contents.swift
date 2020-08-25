190a516a59bcd375ae25221aa873b8bf9068c13d
Andrew Maher <andrew98@Andrews-Mac.local>
2020-08-25 02:29:05 -0700

    Day 1- Variables and Strings

---
+++ Intro To Swift.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Variables and Strings
+var str = "Hello, playground"
+var age = 22
+var population = 8_000_000
+var distance = 3.8
+var isLoaded = true
+
+// Multi line string
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+this goes \
+over mutiple \
+lines
+"""
+
+// String interpolation
+var str3 = "Your distance was \(distance)"
+
+// Constants
+let taylor = "Swift"
+
+// Explicit typing
+let album: String = "Reputation"
+let isReleased: Bool = false
+let rate: Double = 4.5
+let price: Int = 50
+
+

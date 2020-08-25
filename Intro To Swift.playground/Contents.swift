9ece992ef6b80e67a400c28c708733c97b3b7905
Andrew Maher <andrew98@Andrews-Mac.local>
2020-08-25 03:03:11 -0700

    Day 2- Complex Types

--- Intro To Swift.playground/Contents.swift
+++ Intro To Swift.playground/Contents.swift
@@ -9,6 +9,7 @@ var population = 8_000_000
 var distance = 3.8
 var isLoaded = true
 
+// ########## Day 1 ##########
 // Multi line string
 var str1 = """
 This goes
@@ -33,4 +34,65 @@ let isReleased: Bool = false
 let rate: Double = 4.5
 let price: Int = 50
 
+// ########## Day 2 ##########
+// Arrays
+let john = "John Lennon"
+let andrew = "Andrew Maher"
+let george = "George Harrison"
+
+let beatles = [john, andrew, george]
+beatles[0]
+
+let x_axis: [Int] = [2, 4, 8, 16]
+x_axis[3]
+
+// Sets
+let colors = Set(["red", "green", "blue"])
+
+// Tuples
+var name = (first: "Taylor", last: "Swift")
+name.0
+name.last
+
+// Dictionaries
+let heights: [String : Double] = [
+    "Taylor": 1.78,
+    "Ed Sheeran": 1.73
+]
+
+heights["Ed Sheeran"]
+
+heights["Andrew", default:0]
+
+// Empty Collections
+var teams = [String : String]()
+teams["Paul"] = "red"
+
+var results = [Int]()
+
+var words = Set<String>()
+
+// Enums
+enum Result{
+    case success
+    case failure
+}
+let result = Result.success
+
+enum Activity{
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+let activity = Activity.talking(topic: "Swift")
+
+enum Planet: Int{
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planet(rawValue: 3)
 
